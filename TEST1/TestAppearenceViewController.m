143bd6280472a7a79f02c16c28aecc47d0f2098c
abdulsamadsyed <abdul@metawatch.org>
2014-08-12 15:38:20 +0300

    @Test Nslog

--- TEST1/TestAppearenceViewController.m
+++ TEST1/TestAppearenceViewController.m
@@ -19,6 +19,7 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     NSLog(@"View Did load");
+    NSLog(@"Test Nslog");
 }
 
 - (void)didReceiveMemoryWarning
